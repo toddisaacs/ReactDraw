0b2a759cf52229164a099ef721bea5b621210719
Todd Isaacs <teisaacs21w@gmail.com>
2017-04-04 17:13:49 -0400

    added test for isNumeric

---
+++ src/components/util.test.js
@@ -0,0 +1,28 @@
+
+import {isNumeric} from './util';
+
+it('test isNumeric', () => {
+    expect(isNumeric('1')).toBeTruthy();
+    expect(isNumeric('1.2')).toBeTruthy();
+    expect(isNumeric('-1')).toBeTruthy();
+    expect(isNumeric('0')).toBeTruthy();
+    expect(isNumeric('0.55')).toBeTruthy();
+    expect(isNumeric('.5')).toBeTruthy();
+
+    expect(isNumeric(1)).toBeTruthy();
+    expect(isNumeric(-1)).toBeTruthy();
+    expect(isNumeric(-1.2)).toBeTruthy();
+    expect(isNumeric(2.23)).toBeTruthy();
+    expect(isNumeric(0)).toBeTruthy();
+
+    expect(isNumeric('')).toBeFalsy();
+    expect(isNumeric('hello')).toBeFalsy();
+    expect(isNumeric('1.2.3.4')).toBeFalsy();
+    expect(isNumeric('#FFFFFF')).toBeFalsy();
+    expect(isNumeric('9,00.00')).toBeFalsy();
+
+    //Hexidecimal
+    expect(isNumeric('0x23')).toBeTruthy();
+
+
+});
